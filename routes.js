5b302edf37fa8f2f603ac0bc627e4e39edf71f3e
atocchi <andrewtocchi@gmail.com>
2019-09-18 20:03:04 -0700

    --added JSON data to routes

--- routes.js
+++ routes.js
@@ -4,28 +4,52 @@ var path = require("path");
 var app = express();
 var PORT = 3030;
 
+var tables = [{
+  name: "Andrew Tocchi",
+  phone: "530.701.2179",
+  email: "andrewtocchi@gmail.com"
+},
+{
+  name: "Mike Brown",
+phone: "555.555.5555",
+email: "mikebrown@gmail.com",
+}];
+
+var queue = [{
+  name: "Mike Brown",
+  phone: "555.555.5555",
+  email: "mikebrown@gmail.com",
+}];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "rest.html"));
-  });
+app.get("/", function (req, res) {
+  // Welcome to our Restaurant!")
+  res.sendFile(path.join(__dirname, "rest.html"));
+});
 
-app.get("/api/:tables", function(req, res) {
-    let choice = req.params.tables;
-  
-    console.log(choice);
-  
-    for (var i = 0; i < tables.length; i++) {
-      if (choice === tables[i].routeName) {
-        return res.json(tables[i]);
-      }
+app.get("/tables", function (req, res) {
+  // Welcome to our Restaurant!")
+    return res.json(tables);
+
+});
+
+app.get("/api/:table", function (req, res) {
+  0
+  let choice = req.params.table;
+
+  console.log(choice);
+
+  for (var i = 0; i < tables.length; i++) {
+    if (choice === tables[i].phone) {
+      return res.json(tables[i]);
     }
-  
-    return res.json(false);
-  });
+  }
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  return res.json(false);
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
